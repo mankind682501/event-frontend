da5e958167354bcf7afca3faeb14dedbf1f3a66d
mankind682501 <166003195+mankind682501@users.noreply.github.com>
2025-06-21 15:11:19 +0530

    Update serverUrl.js

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,2 @@
-// export const serverUrl="https://event-backend-7x3o.onrender.com"
-export const serverUrl="http://localhost:4000"
+export const serverUrl="https://event-backend-7x3o.onrender.com"
+// export const serverUrl="http://localhost:4000"
